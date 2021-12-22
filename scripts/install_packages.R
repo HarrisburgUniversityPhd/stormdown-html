795aa2cede1e0ef7177bd212f9c6d3a1d3344019
Mark Newman <mark@trinetteandmark.com>
2021-12-22 13:07:03 -0500

    comment

--- scripts/install_packages.R
+++ scripts/install_packages.R
@@ -1,14 +1,23 @@
+# current packages
 ipkgs <- installed.packages()
-pkgs <-
-  c(
-    'knitr',
-    'here',
-    'DiagrammeR',
-    'DiagrammeRsvg',
-    'rsvg')
-for(pkg in pkgs) {
-  if (!(pkg %in% ipkgs)) {
-    install.packages(pkg)
+
+# tables
+pkgs_table <- c('knitr', 'kableExtra')
+# figures
+pkgs_figure <- c('knitr')
+# flowcharts
+pkgs_chart <- c('knitr', 'DiagrammeR', 'DiagrammeRsvg', 'rsvg')
+
+install_needed <- function(pkgs) {
+  for(pkg in pkgs) {
+    if (!(pkg %in% ipkgs)) {
+      install.packages(pkg)
+    }
   }
 }
-rm(ipkgs, pkgs, pkg)
+
+install_needed(pkgs_table)
+install_needed(pkgs_figure)
+install_needed(pkgs_chart)
+
+rm(ipkgs, pkgs_table, pkgs_figure, pkgs_chart, install_needed)
